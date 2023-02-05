0095213b09c0a729e861952594afcc193d95176e
DJAVI <badamshindanil@yandex.ru>
2023-02-05 18:02:01 +0500

    Task 10

---
+++ Task_010/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число: ");
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+    int singleDigit=0;
+    singleDigit=number;
+    singleDigit=singleDigit/10;
+    singleDigit=singleDigit%10;
+    Console.WriteLine(number + " -> " + singleDigit);
+
+}
+else Console.WriteLine("Некорректное число");
