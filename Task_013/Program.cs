67c14b4936e174e31937832f2d8613666fac48fc
DJAVI <badamshindanil@yandex.ru>
2023-02-05 18:30:45 +0500

    Task 13

---
+++ Task_013/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+    if (number > 99)
+    {
+        int num =0;
+        num = number;
+        num = num % 10;
+        Console.WriteLine(number + " -> " + num);
+    }
+    else Console.WriteLine(number + " -> Третьей цифры нет");
+}
+
+else Console.WriteLine("Некорректное число");
